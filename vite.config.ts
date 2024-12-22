23135d271acdf6d11ef3c833fdc7f87c3baf0d5b
Florian <45694132+flo-bit@users.noreply.github.com>
2024-12-22 19:00:55 +0100

    fix deploy?

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), resolve()],
+	build: {
+		rollupOptions: {
+			external: ['@resvg/resvg-js-linux-x64-gnu']
+		}
+	}
 });
